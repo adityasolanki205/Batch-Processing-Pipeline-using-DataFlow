ba9820fcebadb0e470ca339635ef66bbcf6fc45c
Aditya Solanki <56908240+adityasolanki205@users.noreply.github.com>
2021-02-24 22:17:45 +0530

    Update Testing-checkpoint.py

--- .ipynb_checkpoints/Testing-checkpoint.py
+++ .ipynb_checkpoints/Testing-checkpoint.py
@@ -9,13 +9,33 @@ SCHEMA =  'Existing_account:STRING,Duration_month:INTEGER,Credit_history:STRING,
 
 class Split(beam.DoFn):
     def process(self, element):
-        Existing_account,Duration_month,Credit_history,Purpose,Credit_amount,Saving,Employment_duration,Installment_rate,Personal_status,Debtors,Residential_Duration,Property,Age,Installment_plans,Housing,Number_of_credits,Job,Liable_People,Telephone,Foreign_worker,Classification= element.split(' ')
+        Existing_account,  Duration_month,Credit_history,Purpose,Credit_amount,Saving,Employment_duration,Installment_rate,Personal_status,Debtors,Residential_Duration,Property,Age,Installment_plans,Housing,Number_of_credits,Job,Liable_People,Telephone,Foreign_worker,Classification = element.split(' ')
         return [{
-            'Date': Date,
-            'Open': float(Open),
-            'Close': float(Close)
+            'Existing_account': str(Existing_account),
+            'Duration_month': str(Duration_month),
+            'Credit_history': str(Credit_history),
+            'Purpose': str(Purpose),
+            'Credit_amount': str(Credit_amount),
+            'Saving': str(Saving),
+            'Employment_duration':str(Employment_duration),
+            'Installment_rate': str(Installment_rate),
+            'Personal_status': str(Personal_status),
+            'Debtors': str(Debtors),
+            'Residential_Duration': str(Residential_Duration),
+            'Property': str(Property),
+            'Age': str(Age),
+            'Installment_plans':str(Installment_plans),
+            'Housing': str(Housing),
+            'Number_of_credits': str(Number_of_credits),
+            'Job': str(Job),
+            'Liable_People': str(Liable_People),
+            'Telephone': str(Telephone),
+            'Foreign_worker': str(Foreign_worker),
+            'Classification': str(Classification)
         }]
 
+def filter_data(data):
+    return data['Purpose'] !=  'NULL' and Classification !=  'NULL' and 'Property' !=  'NULL' and 'Personal_status' != 'NULL' and 'Existing_account' != 'NULL' and 'Credit_amount' != 'NULL' 
     
 def run(argv=None, save_main_session=True):
     parser = argparse.ArgumentParser()
@@ -34,7 +54,8 @@ def run(argv=None, save_main_session=True):
     with beam.Pipeline(options=PipelineOptions()) as p:
         csv_lines = (p 
                      | beam.io.ReadFromText(known_args.input, skip_header_lines = 1) 
-                     | beam.Pardo(Split())
+                     | beam.ParDo(Split())
+                     | beam.Filter(filter_data)
                      | beam.io.WriteToText(known_args.output)
                     )
         
